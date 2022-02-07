aa09fd894308ee050a0027786b5319512ad7a485
lukeyusuke <lukedev09@gmail.com>
2022-02-07 16:55:33 -0300

    Congela o envio do formulário
    Congela o envio do formulário para que quando o usuário digitar, seus
    dados sejam registrados.

---
+++ assets/js/validacao.js
@@ -0,0 +1,13 @@
+const form = document.querySelector(".form-box");
+
+form.addEventListener('submit', function (recebeFormulario){
+    recebeFormulario.preventDefault();
+
+    const pegaNome = document.getElementById("nome");
+    const pegaCelular = document.getElementById("celular");
+    const pegaEmail = document.getElementById("email");
+    const pegaFaturamento = document.getElementById("faturamento");
+    
+    console.lo
+});
+
