b13b7f48ba43aa975a043e385203aa4f56338012
lukeyusuke <lukedev09@gmail.com>
2022-02-08 17:52:08 -0300

    Cria as estruturas para validação do formulário
    Criação de estruturas condicionais com o intuito de validar cada item
    do formulário

--- assets/js/validacao.js
+++ assets/js/validacao.js
@@ -7,7 +7,42 @@ form.addEventListener('submit', function (recebeFormulario){
     const pegaCelular = document.getElementById("celular");
     const pegaEmail = document.getElementById("email");
     const pegaFaturamento = document.getElementById("faturamento");
-    
-    console.lo
+
+
+    validaNome(pegaNome.value);
+    validaCelular(pegaCelular.value);
+    validaEmail(pegaEmail.value);
+    validaFaturamento(pegaFaturamento.value);
 });
 
+function validaNome(nome){
+    if(nome == ""){
+        const mensagem = document.querySelector("#aviso-nome");
+        msg(mensagem);
+    }
+}
+
+function validaCelular(celular){
+    if(celular == ""){
+        const mensagem = document.querySelector("#aviso-celular");
+        msg(mensagem);
+    }
+}
+
+function validaEmail(email){
+    if(email == ""){
+        const mensagem = document.querySelector("#aviso-email");
+        msg(mensagem);
+    }
+}
+
+function validaFaturamento(email){
+    if(email == ""){
+        const mensagem = document.querySelector("#aviso-faturamento");
+        msg(mensagem);
+    }
+}
+
+function msg(mensagem){
+    mensagem.innerHTML = "Este campo é obrigatório";
+}
